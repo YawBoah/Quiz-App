193e5e649bf4a1ee7ca8046a22f098b6ef5d6b79
YBoah <yawboahene.yb@gmail.com>
2023-10-24 08:55:26 +0000

    More changes

--- script.js
+++ script.js
@@ -122,6 +122,8 @@ const questions = [
     
   ];
   
+
+  
   const questionElement = document.getElementById("question");
   const answerButtons = document.getElementById("answer-buttons");
   const nextButton = document.getElementById("next-btn");
