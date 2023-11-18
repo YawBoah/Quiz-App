d40f65ce5f3bf32974746eee244280da95bb5cdb
YBoah <yawboahene.yb@gmail.com>
2023-11-18 06:06:39 +0000

    More questions

--- script.js
+++ script.js
@@ -8,6 +8,183 @@ const questions = [
         { text: "Cape Coast", correct: false },
       ],
     },
+
+    {
+      question: "What is the official language of Ghana?",
+      answers: [
+        { text: "English", correct: true },
+        { text: "Twi", correct: false },
+        { text: "Akan", correct: false },
+        { text: "Ga", correct: false },
+      ],
+    },
+
+    {
+      question: "Ghana gained independence from British colonial rule in which year?",
+      answers: [
+        { text: "1957", correct: true },
+        { text: "1962", correct: false },
+        { text: "1971", correct: false },
+        { text: "1980", correct: false },
+      ],
+    },
+
+    {
+      question: "What is the largest ethnic group in Ghana?",
+      answers: [
+        { text: "Akan", correct: true },
+        { text: "Mole-Dagbon", correct: false },
+        { text: "Mande", correct: false },
+        { text: "Ewe", correct: false },
+      ],
+    },
+
+    {
+      question: "Which river is the longest in Ghana?",
+      answers: [
+        { text: "Volta River", correct: true },
+        { text: "Pra River", correct: false },
+        { text: "Ankobra River", correct: false },
+        { text: "Tano River", correct: false },
+      ],
+    },
+
+    {
+      question: "In which year did Ghana host the first African Cup of Nations (AFCON)?",
+      answers: [
+        { text: "1963", correct: true },
+        { text: "1972", correct: false },
+        { text: "1985", correct: false },
+        { text: "1999", correct: false },
+      ],
+    },
+    {
+      question: "Who was the first President of Ghana?",
+      answers: [
+        { text: "Kwame Nkrumah", correct: true },
+        { text: "John Agyekum Kufuor", correct: false },
+        { text: "Jerry John Rawlings", correct: false },
+        { text: "John Dramani Mahama", correct: false },
+      ],
+    },
+
+    {
+      question: "Which famous UNESCO World Heritage site is located in Cape Coast, Ghana?",
+      answers: [
+        { text: "Kakum National Park", correct: false },
+        { text: "Wli Waterfalls", correct: false },
+        { text: "Elmina Castle", correct: true },
+        { text: "Mole National Park", correct: false },
+      ],
+    },
+
+    {
+      question: "What is the traditional cloth of the Akan people in Ghana?",
+      answers: [
+        { text: "Kente", correct: true },
+        { text: "Batakari", correct: false },
+        { text: "Smock", correct: false },
+        { text: "Fugu", correct: false },
+      ],
+    },
+
+    {
+      question: "Which festival is celebrated by the people of the Ga-Dangme ethnic group?",
+      answers: [
+        { text: "Homowo", correct: true },
+        { text: "Aboakyere", correct: false },
+        { text: "Damba", correct: false },
+        { text: "Odwira", correct: false },
+      ],
+    },
+    {
+      question: "What is the main export product of Ghana?",
+      answers: [
+        { text: "Cocoa", correct: true },
+        { text: "Gold", correct: false },
+        { text: "Oil", correct: false },
+        { text: "Timber", correct: false },
+      ],
+    },
+
+    {
+      question: "Which traditional symbol represents unity in the Akan culture?",
+      answers: [
+        { text: "Adinkra", correct: false },
+        { text: "Sankofa", correct: false },
+        { text: "Gye Nyame", correct: true },
+        { text: "Akoben", correct: false },
+      ],
+    },
+
+    {
+      question: "Who is the national football team of Ghana?",
+      answers: [
+        { text: "Black Stars", correct: true },
+        { text: "Golden Eagles", correct: false },
+        { text: "Black Queens", correct: false },
+        { text: "Black Meteors", correct: false },
+      ],
+    },
+
+    {
+      question: "Which region of Ghana is known for its rich fantasy coffin tradition?",
+      answers: [
+        { text: "Greater Accra Region", correct: true },
+        { text: "Ashanti Region", correct: false },
+        { text: "Volta Region", correct: false },
+        { text: "Central Region", correct: false },
+      ],
+    },
+
+    {
+      question: "In which year did Ghana become a republic?",
+      answers: [
+        { text: "1957", correct: false },
+        { text: "1960", correct: true },
+        { text: "1971", correct: false },
+        { text: "1984", correct: false },
+      ],
+    },
+
+    {
+      question: "Which popular dance originated from the Akan people of Ghana?",
+      answers: [
+        { text: "Azonto", correct: false },
+        { text: "Kpanlogo", correct: true },
+        { text: "Shoki", correct: false },
+        { text: "Alkayida", correct: false },
+      ],
+    },
+    {
+      question: "What is the meaning of the name 'Ghana'?",
+      answers: [
+        { text: "Land of Gold", correct: true },
+        { text: "Great Kingdom", correct: false },
+        { text: "Black Star", correct: false },
+        { text: "Warrior People", correct: false },
+      ],
+    },
+
+    {
+      question: "Which waterfall is the highest in Ghana?",
+      answers: [
+        { text: "Boti Falls", correct: false },
+        { text: "Tagbo Falls", correct: false },
+        { text: "Wli Waterfalls", correct: true },
+        { text: "Kintampo Waterfalls", correct: false },
+      ],
+    },
+
+    {
+      question: "What is the name of the traditional warrior dance of the Ashanti people?",
+      answers: [
+        { text: "Azonto", correct: false },
+        { text: "Adowa", correct: true },
+        { text: "Kpanlogo", correct: false },
+        { text: "Borborbor", correct: false },
+      ],
+    },
   
 
     {
@@ -27,16 +204,6 @@ const questions = [
         { text: "Black Stars", correct: true },
         { text: "Red Elephants", correct: false },
         { text: "Cape Stars", correct: false },
-      ],
-    },
-
-    {
-      question: "Who is the current captain of the Ghanaian senior national Football team?",
-      answers: [
-        { text: "Asamoah Gyan", correct: false },
-        { text: "Stephen Appiah", correct: false },
-        { text: "John Mensah", correct: false },
-        { text: "Dede Ayew", correct: true },
       ],
     },
 
@@ -118,9 +285,7 @@ const questions = [
         { text: "Jerry John Rawlings", correct: false },
         { text: "Nana Addo Dankwa Akufo-Addo", correct: false },
       ],
-    },
-
-    
+    }, 
   ];
   
 
